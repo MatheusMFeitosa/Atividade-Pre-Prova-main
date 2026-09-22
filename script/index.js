b029334110fb91d8a4ceaf88911e4e2778facfa5
MatheusMFeitosa <matheusmf.feitosa@gmail.com>
2026-09-22 20:58:26 -0300

    Adição de parte do estilo das noticias laterais e sua lógica de numeros com before

--- script/index.js
+++ script/index.js
@@ -14,3 +14,17 @@ botaoMenu.addEventListener("click", alternarMenu);
 
 // para que o usuario clique em qualquer lugar e saia do menu tambem
 overlay.addEventListener("click", alternarMenu);
+
+
+// function mundarNumeroNoticia() {
+//     const numeroOutrasNoticias = document.querySelectorAll(".linha-noticia");
+
+//     for (let i = 0; i < numeroOutrasNoticias.length; i++) {
+//         const numero = numeroOutrasNoticias[i];
+
+//         numero.style.setProperty("--texto-before", `"${i}"`);
+        
+//     }
+// }
+
+// mundarNumeroNoticia();
